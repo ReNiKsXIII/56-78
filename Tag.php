cd4da411e11f16373d58e343c9aa430c247ed992
Tihon <tihon123123@yandex.ru>
2022-06-20 02:19:58 +0700

    65

--- Tag.php
+++ Tag.php
@@ -1,21 +1,41 @@
-				$result = '';
-
-				foreach ($attrs as $name => $value) {
-					$result .= " $name=\"$value\"";
-					// Если значение атрибута равно true:
-					if ($value === true) {
-						$result .= " $name"; // это атрибут без значения
-					} else {
-						$result .= " $name=\"$value\""; // это атрибут со значением
-					}
-				}
-
-				return $result;
-@@ -57,5 +62,7 @@ public function setAttrs($attrs)
-			}	
-			return $this;
+		return $this;
 		}
 
+		public function addClass($className)
+		{
+			if (isset($this->attrs['class'])) {
+				$classNames = explode(' ', $this->attrs['class']);
 
+				if (!in_array($className, $classNames)) {
+					$classNames[] = $className;
+					$this->attrs['class'] = implode(' ', $classNames);
+				}
+			} else {
+				$this->attrs['class'] = $className;
+			}
+
+			return $this;
+		}		
+
+		private function removeElem($elem, $arr)
+		{
+			$key = array_search($elem, $arr); // находим ключ элемента по его тексту
+			array_splice($arr, $key, 1); // удаляем элемент
+			return $arr; // возвращаем измененный массив
+		}
+
+		public function removeClass($className)
+		{
+			if (isset($this->attrs['class'])) {
+				$classNames = explode(' ', $this->attrs['class']);
+
+				if (in_array($className, $classNames)) {
+					$classNames = $this->removeElem($className, $classNames);
+					$this->attrs['class'] = implode(' ', $classNames);
+				}
+			}
+
+			return $this;
+		}		
 	}
 ?>
--- index.php
+++ index.php
@@ -1,15 +1,16 @@
+<?php
+	require_once 'Tag.php';
 
 	$tag = new Tag('input');
 
-	echo $tag
-		->setAttr('id', 'test')
-		->setAttr('disabled', true)
-		->open();
-
-		// выведет <input id="test" disabled>
 	echo (new Tag('input'))->setAttr('name', 'name1')->open();
 	echo (new Tag('input'))->setAttr('name', 'name2')->open();
 
 	//вывод <input name="name1"><input name="name2">
+	echo (new Tag('input'))
+		->setAttr('class', 'eee zzz kkk') // добавим 3 класса
+		->removeClass('zzz') // удалим класс 'zzz'
+		->addClass('test')
+		->open(); // выведет <input class="eee kkk">
 
 ?> 
