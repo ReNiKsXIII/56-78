d75229546ebdf6061811a2a1b6711816df24f15d
Tihon <tihon123123@yandex.ru>
2022-06-20 02:19:36 +0700

    64

--- index.php
+++ index.php
@@ -1,16 +1,15 @@
-<?php
-	require_once 'Tag.php';
+
 	$tag = new Tag('input');
 
 	echo $tag
-		->setAttrs(['id' => 'test', 'class' => 'eee'])
-		->setAttr('type', 'text')
-		->open(); 
 		->setAttr('id', 'test')
 		->setAttr('disabled', true)
 		->open();
 
-		// выведет <input id="test" class="eee" type="text">
 		// выведет <input id="test" disabled>
+	echo (new Tag('input'))->setAttr('name', 'name1')->open();
+	echo (new Tag('input'))->setAttr('name', 'name2')->open();
+
+	//вывод <input name="name1"><input name="name2">
 
 ?> 
