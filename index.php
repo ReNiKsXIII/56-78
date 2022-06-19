6f6ba116381952b96ede732fe480687368c099e8
Tihon <tihon123123@yandex.ru>
2022-06-20 02:20:18 +0700

    66

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
 <?php
 	require_once 'Tag.php';
 
-	$tag = new Tag('input');
-
-	echo (new Tag('input'))->setAttr('name', 'name1')->open();
-	echo (new Tag('input'))->setAttr('name', 'name2')->open();
-
-	//вывод <input name="name1"><input name="name2">
 	echo (new Tag('input'))
 		->setAttr('class', 'eee zzz kkk') // добавим 3 класса
 		->removeClass('zzz') // удалим класс 'zzz'
 		->addClass('test')
 		->open(); // выведет <input class="eee kkk">
+	$tag = new Tag('input');
+
+	$tag->setAttr('class', 'eee zzz kkk'); // добавим 3 класса
+	$tag->removeClass('zzz'); // удалим класс 'zzz'
+	$tag->addClass('test');
+	echo nl2br($tag->open()."\n\n getName: "); // выведет <input class="eee kkk">
+	echo nl2br($tag->getName()."\n getAttrs: ");
+	echo nl2br($tag->getAttrs()."\n getAttr: ");
+	echo nl2br($tag->getAttr('class')."\n getText: ");
+	echo nl2br($tag->getText());
+
 
 ?> 
