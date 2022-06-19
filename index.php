b8d5419cee884bb0afe25d7102aeb995b6be0442
Tihon <tihon123123@yandex.ru>
2022-06-20 02:15:10 +0700

    59

--- index.php
+++ index.php
@@ -1,22 +1,62 @@
 <?php
+	function FileSizeConvert($bytes)
+	{
+		$bytes = floatval($bytes);
+			$arBytes = array(
+				0 => array(
+					"UNIT" => "TB",
+					"VALUE" => pow(1024, 4)
+				),
+				1 => array(
+					"UNIT" => "GB",
+					"VALUE" => pow(1024, 3)
+				),
+				2 => array(
+					"UNIT" => "MB",
+					"VALUE" => pow(1024, 2)
+				),
+				3 => array(
+					"UNIT" => "KB",
+					"VALUE" => 1024
+				),
+				4 => array(
+					"UNIT" => "B",
+					"VALUE" => 1
+				),
+			);
+
+		foreach($arBytes as $arItem)
+		{
+			if($bytes >= $arItem["VALUE"])
+			{
+				$result = $bytes / $arItem["VALUE"];
+				$result = str_replace(".", "," , strval(round($result, 2)))." ".$arItem["UNIT"];
+				break;
+			}
+		}
+		return $result;
+	}
+
+
 	header('Content-Type: text/html; charset=utf-8');
-	require_once "date.php";
+	require_once 'IFile.php';
+	require_once 'File.php';
+	require_once 'Tag.php';
+	$tag = new Tag('h3');
 
-	$date = new Date('2025-12-31');	
-	echo $date->getYear().nl2br("\n");  
-	echo $date->getMonth('ru').nl2br("\n"); 
-	echo $date->getMonth('en').nl2br("\n"); 
-	echo $date->getMonth().nl2br("\n"); 
-	echo $date->getMonth('haha').nl2br("\n"); 
-	echo $date->getDay().nl2br("\n\n");
+	$file = new File('ntoskrnl.exe');
+	print(nl2br("FilePath: ".$file->getPath()."\n"));
+	print(nl2br("FileDir: ".$file->getDir()."\n"));
+	print(nl2br("FileName: ".$file->getName()."\n"));
+	print(nl2br("FileExt: ".$file->getExt()."\n"));
+	print(nl2br("FileSize: ".FileSizeConvert($file->getSize())."\n"));
+	print(nl2br("FileText: не вывожу, файл binary\n"));
+	echo $tag->Open().'text'.$tag->Close(); // выведет <div>text</div>
 
-	echo $date->getWeekDay().nl2br("\n");   
-	echo $date->getWeekDay('ru').nl2br("\n"); 
-	echo $date->getWeekDay('en').nl2br("\n"); 
-	echo $date->getWeekDay('haha').nl2br("\n\n"); 
+	#print(nl2br("\n\nFileText: \n\n".$file->getText()."\n"));
+	# не вывожу, файл binary.
 
-	echo (new Date('2025-12-31'))->addYear(1).nl2br("\n");
-	echo (new Date('2025-12-31'))->addDay(1).nl2br("\n");  
-	echo (new Date('2025-12-31'))->subDay(3).nl2br("\n"); 
-	echo (new Date('2025-12-31'))->addYear(1).nl2br("\n"); 
+
+
 ?>
+?> 
