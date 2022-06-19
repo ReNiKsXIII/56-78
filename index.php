b44814bb41b80bf29a7115bcce26a5fd2316f9e3
Tihon <tihon123123@yandex.ru>
2022-06-20 02:27:58 +0700

    71

--- index.php
+++ index.php
@@ -1,46 +1,21 @@
-<?php
-	require_once 'iTag.php';
-	require_once 'Tag.php';
-	require_once 'Link.php';
-	require_once 'menu.php';
+	require_once 'Password.php';
+	require_once 'Hidden.php';
+	require_once 'Textarea.php';
 
-	print(nl2br($link1."\n"));	
-	print(nl2br($link2."\n"));	
-	print(nl2br($link3."\n"));	
-	print(nl2br($link4."\n"));	
-	print(nl2br($link5."\n"));	
-	print(nl2br($link6."(show)\n"));	
-	require_once 'ListItem.php';
-	require_once 'HtmlList.php';
-	require_once 'Ol.php';
-	require_once 'Ul.php';
+	require_once 'Checkbox.php';
 
-	echo (new Link)
-		->setAttr('href', 'menu/5.php')
-		->setAttr('class', 'link1 link2') // добавляем ссылке классы
-		->setText('index')
-		->show();
-	$list = new HtmlList('ul');
-	$ul = new Ul; 
-	$ol = new Ol;	
+	$form = (new Form)->setAttrs([
+		'action' => 'test.php',
+		'method' => 'GET'
+@@ -18,6 +19,11 @@
+		echo (new Input)->setAttr('name', 'login');
+		echo (new Password)->setAttr('name', 'passw');
+		echo (new Textarea)->setAttr('name', 'message')->show();
+		echo nl2br("\n");
+		echo " Checkbox: ".(new Checkbox)->setAttr('name', 'checkbox');
+		echo nl2br("\n\n\n");
 
-	$list->addItem((new ListItem())->setText('item1'));
-	$list->addItem((new ListItem())->setText('item2'));
-	$list->addItem((new ListItem())->setText('item3'));	
-	echo $list; // не вызываем open 
 
-	echo nl2br("\n");
-
-	$ol->addItem((new ListItem())->setText('item1'));
-	$ol->addItem((new ListItem())->setText('item2'));
-	$ol->addItem((new ListItem())->setText('item3'));
-	echo $ol;
-
-	echo nl2br("\n");
-
-	$ul->addItem((new ListItem())->setText('item1'));
-	$ul->addItem((new ListItem())->setText('item2'));
-	$ul->addItem((new ListItem())->setText('item3'));
-	echo $ul;
-
+		echo new Submit;
+	echo $form->close();
 ?> 
