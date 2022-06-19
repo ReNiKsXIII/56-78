7dd1f9635c6520ad7b362debff88333b47982c73
Tihon <tihon123123@yandex.ru>
2022-06-20 02:29:10 +0700

    72

---
+++ Input.php
@@ -0,0 +1,13 @@
+<?php
+	class Input extends Tag
+	{
+		public function __construct()
+		{
+			parent::__construct('input');
+		}
+		public function __toString()
+		{
+			return parent::open();
+		}
+	}
+?> 
--- index.php
+++ index.php
@@ -1,21 +1,20 @@
-	require_once 'Password.php';
-	require_once 'Hidden.php';
-	require_once 'Textarea.php';
-
-	require_once 'Checkbox.php';
+	require_once 'iTag.php';
+	require_once 'Tag.php';
+	require_once 'Form.php';
+	require_once 'Input.php';
 
 	$form = (new Form)->setAttrs([
 		'action' => 'test.php',
-		'method' => 'GET'
-@@ -18,6 +19,11 @@
-		echo (new Input)->setAttr('name', 'login');
-		echo (new Password)->setAttr('name', 'passw');
-		echo (new Textarea)->setAttr('name', 'message')->show();
-		echo nl2br("\n");
-		echo " Checkbox: ".(new Checkbox)->setAttr('name', 'checkbox');
-		echo nl2br("\n\n\n");
+		'method' => 'POST'
+	]);
+	$form = (new Form)->setAttrs(['action' => '', 'method' => 'GET']);
 
-
-		echo new Submit;
+	echo $form->open();
+		// здесь потом будут элементы формы
+	echo (new Input)->setAttr('name', 'year')->open();
+	echo (new Input)->setAttr('type', 'submit')->open();
 	echo $form->close();
+
+	//выводит <form action="test.php" method="POST"></form>
+
 ?> 
