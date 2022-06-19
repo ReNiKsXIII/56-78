4c8ad38541f637cf3b535524a9dc756f7c45add3
Tihon <tihon123123@yandex.ru>
2022-06-20 02:19:18 +0700

    63

--- Tag.php
+++ Tag.php
@@ -1,13 +1,21 @@
-		return '';
-			}
-		}
+				$result = '';
 
-		public function setAttrs($attrs)
-		{
-			foreach ($attrs as $name => $value) {
-				$this->setAttr($name, $value);
+				foreach ($attrs as $name => $value) {
+					$result .= " $name=\"$value\"";
+					// Если значение атрибута равно true:
+					if ($value === true) {
+						$result .= " $name"; // это атрибут без значения
+					} else {
+						$result .= " $name=\"$value\""; // это атрибут со значением
+					}
+				}
+
+				return $result;
+@@ -57,5 +62,7 @@ public function setAttrs($attrs)
 			}	
 			return $this;
 		}
+
+
 	}
 ?>
--- index.php
+++ index.php
@@ -1,20 +1,16 @@
 <?php
 	require_once 'Tag.php';
-
-	$tag = new Tag('div');
-	$tag->setAttr('id', 'test'); // откроем тег
-	$tag->removeAttr('id'); // удалим тег
-	echo $tag->Open();
-	echo 'sadas';
-	echo $tag->Close(); // закроем тег
 	$tag = new Tag('input');
 
-	// выводит <div>sadas</div>
 	echo $tag
 		->setAttrs(['id' => 'test', 'class' => 'eee'])
 		->setAttr('type', 'text')
 		->open(); 
+		->setAttr('id', 'test')
+		->setAttr('disabled', true)
+		->open();
 
 		// выведет <input id="test" class="eee" type="text">
+		// выведет <input id="test" disabled>
 
 ?> 
