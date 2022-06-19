1c088187e079513899726c0b7dfb76aa3940b6f1
Tihon <tihon123123@yandex.ru>
2022-06-20 02:17:22 +0700

    59

--- Tag.php
+++ Tag.php
@@ -1,20 +1,43 @@
-<?php
-	class Tag
-	{
-		private $name;
-
-		public function __construct($name)
-		{
 			$this->name = $name;
 		}
 
 		public function Open()
+		public function Open($params = null)
 		{
 			return "<$this->name>";
+			if($this->name == "img")
+			{
+				return "<$this->name src='$params'";	
+			}
+
+			if($params == "header")
+			{
+				return "<header><$this->name>";
+			}
+
+			if(empty($params))
+			{
+				return "<$this->name>";	
+			}
 		}
 		public function Close()
+		public function Close($params = null)
 		{
 			return "</$this->name>";
+			if($this->name == "img")
+			{
+				return ">";	
+			}
+
+			if($params == "header")
+			{
+				return "</$this->name></header>";
+			}
+
+			if(empty($params))
+			{
+				return "</$this->name>";	
+			}
 		}		
 	}
 ?>
--- index.php
+++ index.php
@@ -1,62 +1,14 @@
 <?php
-	function FileSizeConvert($bytes)
-	{
-		$bytes = floatval($bytes);
-			$arBytes = array(
-				0 => array(
-					"UNIT" => "TB",
-					"VALUE" => pow(1024, 4)
-				),
-				1 => array(
-					"UNIT" => "GB",
-					"VALUE" => pow(1024, 3)
-				),
-				2 => array(
-					"UNIT" => "MB",
-					"VALUE" => pow(1024, 2)
-				),
-				3 => array(
-					"UNIT" => "KB",
-					"VALUE" => 1024
-				),
-				4 => array(
-					"UNIT" => "B",
-					"VALUE" => 1
-				),
-			);
-
-		foreach($arBytes as $arItem)
-		{
-			if($bytes >= $arItem["VALUE"])
-			{
-				$result = $bytes / $arItem["VALUE"];
-				$result = str_replace(".", "," , strval(round($result, 2)))." ".$arItem["UNIT"];
-				break;
-			}
-		}
-		return $result;
-	}
-
-
-	header('Content-Type: text/html; charset=utf-8');
-	require_once 'IFile.php';
-	require_once 'File.php';
 	require_once 'Tag.php';
 	$tag = new Tag('h3');
 
-	$file = new File('ntoskrnl.exe');
-	print(nl2br("FilePath: ".$file->getPath()."\n"));
-	print(nl2br("FileDir: ".$file->getDir()."\n"));
-	print(nl2br("FileName: ".$file->getName()."\n"));
-	print(nl2br("FileExt: ".$file->getExt()."\n"));
-	print(nl2br("FileSize: ".FileSizeConvert($file->getSize())."\n"));
-	print(nl2br("FileText: не вывожу, файл binary\n"));
 	echo $tag->Open().'text'.$tag->Close(); // выведет <div>text</div>
+	$tag0 = new Tag('div');
+	$tag1 = new Tag('h2');
+	$tag2 = new Tag('img');
 
-	#print(nl2br("\n\nFileText: \n\n".$file->getText()."\n"));
-	# не вывожу, файл binary.
+	echo $tag0->Open().'text'.$tag0->Close(); // выведет <div>text</div>
+	echo $tag1->Open("header").'text'.$tag1->Close("header"); // выведет <header><h2>text</h2></header>
+	echo $tag2->Open("cat-005.png").$tag2->Close(); // выведет <img src="cat-005.png">
 
-
-
-?>
 ?> 
